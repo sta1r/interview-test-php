f3a08975e6b23df879c262b01c6da07f5a602fca
Simon Letch <Simon.Letch@dotdigital.com>
2020-03-18 21:08:28 +0000

    Simple calculator skeleton with tests

---
+++ SimpleCalculator.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This class encapsulates the logic to return the result of a calculation represented as a string
+ *
+ * It only supports addition and subtraction
+ * It does not support multiplication, division, brackets or indices
+ *
+ */
+class SimpleCalculator
+{
+    /**
+     * Returns the result of a calculation represented as a string
+     *
+     * @param string $calculation
+     * @return int
+     * @throws Exception
+     */
+    public static function calculate(string $calculation) : int
+    {
+        throw new Exception("Not implemented");
+    }
+}
---
+++ SimpleCalculatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("SimpleCalculator.php");
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleCalculatorTest extends TestCase
+{
+    public function testAddition()
+    {
+        $this->assertEquals(2, SimpleCalculator::calculate("1 + 1"));
+    }
+
+    public function testSubtraction()
+    {
+        $this->assertEquals(2, SimpleCalculator::calculate("1 + 1"));
+    }
+
+    public function testAdditionsAndSubtractions()
+    {
+        $this->assertEquals(50,  SimpleCalculator::calculate("50 - 45 + 45"));
+        $this->assertEquals(80,  SimpleCalculator::calculate("1 + 5 + 100 - 26"));
+        $this->assertEquals(2,  SimpleCalculator::calculate("1 + 1 + 1 - 1 - 1 + 1"));
+    }
+
+    public function testNegativeResult()
+    {
+        $this->assertEquals(-100, SimpleCalculator::calculate("100 - 200"));
+    }
+}
